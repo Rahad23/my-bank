f68d90023c18c96aea36fdbe416092201247b3f1
Rahad2u <94119962+Rahad2u@users.noreply.github.com>
2022-08-16 17:32:45 +0600

    Bank calculation

---
+++ index.js
@@ -0,0 +1,20 @@
+// Login js
+document.getElementById('login-button').addEventListener('click', function(){
+
+    const myEmail = document.getElementById('login-email');
+    const inputEmailValue = myEmail.value;
+    
+    const myPassword = document.getElementById('login-password');
+   
+    const inputPassword = myPassword.value;
+if(inputEmailValue === "rahadhasan3367@gmail.com" && inputPassword === "rahad@@"){
+    location.href = './bank.html';
+}else{
+    alert('Your email and password not match');
+}
+myEmail.value = "";
+myPassword.value = "";
+});
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  module.exports = {
+    theme: {
+      extend: {
+        animation: {
+          wiggle: 'wiggle 1s ease-in-out infinite',
+        }
+      }
+    }
+  }
